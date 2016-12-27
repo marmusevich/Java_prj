a618362dc5c01e7808132914c6f9e8bb36a24c5e
marmusevich <myrmik@mail.com>
2016-12-27 15:08:16 +0200

    log

--- NWProtocol/src/main/java/protocol/Parameters.java
+++ NWProtocol/src/main/java/protocol/Parameters.java
@@ -1,5 +1,7 @@
 package protocol;
 
+import org.firebirdsql.pool.FBConnectionPoolDataSource;
+
 import java.nio.charset.Charset;
 
 /**
@@ -48,22 +50,88 @@ public class Parameters {
     public long commandAdTimeout = 10;
 
     // ---------- ГРУППА БД -------------------
+    //TerminalDB
+    /**
+     * максимальное количество подключений к базе
+     */
+    public int terminalDBMaxPoolSize = 5;
+    /**
+     * минимальное количество подключений
+     */
+    public int terminalDBMinPoolSize = 2;
+    /**
+     * количество закешированых запросов в одном подключении
+     */
+    public int terminalDBMaxStatements = 10;
+    /**
+     * тайм аут простоя подключенийя в секундах
+     */
+    public int terminalDBMaxIdleTime = 30 * 60;
     /**
      * кодировка для сообщений в базе
      */
-    public Charset bdCharset = Charset.forName("windows-1251");
+    public Charset terminalDBCharset = Charset.forName("windows-1251");
+    /**
+     * диалект SQL
+     */
+    public String terminalDBSqlDialect = "3";
+    /**
+     * строка подключения к базе
+     */
+    public String terminalDBDatabase = "localhost/3050:E:/a.marmusevich/TERMINAL/TERMINAL.FDB";
+    /**
+     * имя пользователя
+     */
+    public String terminalDBUserName = "SYSDBA";
+    /**
+     * пароль
+     */
+    public String terminalDBPassword = "masterkey";
+
+    //Working DB
+    /**
+     * максимальное количество подключений к базе
+     */
+    public int workingDBMaxPoolSize = 5;
+    /**
+     * количество одновременных запросов в одном подключении
+     */
+    public int workingDBMinPoolSize = 2;
+    /**
+     * количество закешированых запросов в одном подключении
+     */
+    public int workingDBMaxStatements  = 10;
+    /**
+     * тайм аут простоя подключенийя в секундах
+     */
+    public int workingDBMaxIdleTime = 30 * 60;
+    /**
+     * кодировка для сообщений в базе
+     */
+    public Charset workingDBCharset = Charset.forName("windows-1251");
+    /**
+     * диалект SQL
+     */
+    public String workingDBSqlDialect = "3";
+    /**
+     * строка подключения к базе
+     */
+    public String workingDBDatabase = "localhost/3050:E:/a.marmusevich/WORKING/WORKING.FDB";
+    /**
+     * имя пользователя
+     */
+    public String workingDBUserName = "SYSDBA";
+    /**
+     * пароль
+     */
+    public String workingDBPassword = "masterkey";
 
 
-//    setMaxPoolSize(5);
-//    setMinPoolSize(2);
-//    setMaxStatements(10);
-//    setMaxIdleTime(30 * 60 * 60);
-//    setEncoding("WIN1251"); bdCharset.toString()
-//    setSqlDialect("3");
-//1 база    setDatabase("localhost/3050:E:/a.marmusevich/TERMINAL/TERMINAL.FDB");
-//2 база    setDatabase("localhost/3050:E:/a.marmusevich/WORKING/WORKING.FDB");
-//    setUserName("SYSDBA");
-//    setPassword("masterkey");
+
+    //1 база    setDatabase("localhost/3050:E:/a.marmusevich/TERMINAL/TERMINAL.FDB");
+    //2 база    setDatabase("localhost/3050:E:/a.marmusevich/WORKING/WORKING.FDB");
+        //E:\a.marmusevich\TERMINAL\TERMINAL.FDB
+        //E:\a.marmusevich\WORKING\WORKING.FDB
 
 
 
@@ -73,9 +141,5 @@ public class Parameters {
      */
     public Parameters(){
     //TODO настройки загрузить откудото или переопределить
-
-
-
     }
-
 }
--- NWProtocol/src/main/java/protocol/bd/DBContext.java
+++ NWProtocol/src/main/java/protocol/bd/DBContext.java
@@ -3,6 +3,7 @@ package protocol.bd;
 //import org.firebirdsql.*;
 
 import org.firebirdsql.pool.*;
+import protocol.Parameters;
 
 import javax.sql.PooledConnection;
 import java.sql.Connection;
@@ -14,109 +15,74 @@ import java.sql.SQLException;
  * контекст базы данных, т.е. здесь подключение и еще что то
  */
 public class DBContext {
+    // ссылки на пулы
+    static private PooledConnection pooledConToTerminalDB = null;
+    static private PooledConnection pooledConToWorkingDB = null;
 
-    //E:\a.marmusevich\TERMINAL\TERMINAL.FDB
-    //E:\a.marmusevich\WORKING\WORKING.FDB
+    /**
+     * инициация подключений к базе данных
+     * @param parameters
+     * @throws Exception
+     */
+    public static void init(Parameters parameters) throws Exception{
+        //todo заменить пул соеденений
 
-    // заглушка
-
-    //todo заменить пул соеденений
-
-//пример подключения с пулом
-    public static void main(String[] args) throws Exception {
-        org.firebirdsql.pool.FBConnectionPoolDataSource pool = new org.firebirdsql.pool.FBConnectionPoolDataSource();
-//        org.firebirdsql.pool.FBPooledConnection pool = new org.firebirdsql.pool.FBPooledConnection();
-
-        pool.setMaxPoolSize(5);
-        pool.setMinPoolSize(2);
-        pool.setMaxStatements(10);
-        pool.setMaxIdleTime(30 * 60 * 60);
-        pool.setEncoding("WIN1251");
-        //pool.setSqlDialect("3");
-        pool.setDatabase("localhost/3050:E:/a.marmusevich/TERMINAL/TERMINAL.FDB");
-        //pool.setDatabase("localhost/3050:E:/a.marmusevich/WORKING/WORKING.FDB");
-        pool.setUserName("SYSDBA");
-        pool.setPassword("masterkey");
+        // Terminal DB
+        org.firebirdsql.pool.FBConnectionPoolDataSource poolToTerminalDB = new FBConnectionPoolDataSource();
+        poolToTerminalDB.setMaxPoolSize(parameters.terminalDBMaxPoolSize);
+        poolToTerminalDB.setMinPoolSize(parameters.terminalDBMinPoolSize);
+        poolToTerminalDB.setMaxStatements(parameters.terminalDBMaxStatements);
+        poolToTerminalDB.setMaxIdleTime(parameters.terminalDBMaxIdleTime);
+        poolToTerminalDB.setEncoding(parameters.terminalDBCharset.name());
+        poolToTerminalDB.setSqlDialect(parameters.terminalDBSqlDialect);
+        poolToTerminalDB.setDatabase(parameters.terminalDBDatabase);
+        poolToTerminalDB.setUserName(parameters.terminalDBUserName);
+        poolToTerminalDB.setPassword(parameters.terminalDBPassword);
         // obtain a physical connection to the database
-        PooledConnection pooledCon = pool.getPooledConnection();
-        // obtain a wrapped connection
-        Connection connection = pooledCon.getConnection();
-        try {
-
-            PreparedStatement ps = connection.prepareStatement("SELECT * FROM USERS_TERM_STATE");
-//            ps.setInt(1, 1);
-//                PreparedStatement ps = connection.prepareStatement("SELECT * FROM USERS_TERM_STATE WHERE id = ?");
-//                ps.setInt(1, 1);
-                ResultSet rs = ps.executeQuery();
-                while (rs.next()) {
-                    System.out.println("LOGIN = " + rs.getString("LOGIN") + ", USERNAME = " + rs.getString("USERNAME"));
-                }
-        } finally {
-            // release the connection back to pool
-            connection.close();
-        }
-
-    }
+        pooledConToTerminalDB = poolToTerminalDB.getPooledConnection();
 
 
-    PooledConnection pooledCon;
+        //Working DB
 
-    public  void init() throws Exception {
-        org.firebirdsql.pool.FBConnectionPoolDataSource pool = new FBConnectionPoolDataSource();
-//        org.firebirdsql.pool.FBPooledConnection pool = new org.firebirdsql.pool.FBPooledConnection();
-
-        pool.setMaxPoolSize(5);
-        pool.setMinPoolSize(2);
-        pool.setMaxStatements(10);
-        pool.setMaxIdleTime(30 * 60 * 60);
-        pool.setEncoding("WIN1251");
-        //pool.setSqlDialect("3");
-        pool.setDatabase("localhost/3050:E:/a.marmusevich/TERMINAL/TERMINAL.FDB");
-        //pool.setDatabase("localhost/3050:E:/a.marmusevich/WORKING/WORKING.FDB");
-        pool.setUserName("SYSDBA");
-        pool.setPassword("masterkey");
+        org.firebirdsql.pool.FBConnectionPoolDataSource poolToWorkingDB = new FBConnectionPoolDataSource();
+        poolToWorkingDB.setMaxPoolSize(parameters.workingDBMaxPoolSize);
+        poolToWorkingDB.setMinPoolSize(parameters.workingDBMinPoolSize);
+        poolToWorkingDB.setMaxStatements(parameters.workingDBMaxStatements);
+        poolToWorkingDB.setMaxIdleTime(parameters.workingDBMaxIdleTime);
+        poolToWorkingDB.setEncoding(parameters.workingDBCharset.name());
+        poolToWorkingDB.setSqlDialect(parameters.workingDBSqlDialect);
+        poolToWorkingDB.setDatabase(parameters.workingDBDatabase);
+        poolToWorkingDB.setUserName(parameters.workingDBUserName);
+        poolToWorkingDB.setPassword(parameters.workingDBPassword);
         // obtain a physical connection to the database
-        pooledCon = pool.getPooledConnection();
-
+        pooledConToWorkingDB = poolToWorkingDB.getPooledConnection();
     }
 
-    public Connection getConnection() throws SQLException {
-        return pooledCon.getConnection();
+    /**
+     * получить подключение к Terminal
+     * @return
+     * @throws SQLException
+     */
+    static public Connection getConnectionToTerminalDB() throws SQLException {
+        if(pooledConToTerminalDB != null)
+                return pooledConToTerminalDB.getConnection();
+
+        return null;
+    }
+
+    /**
+     * получить подключение к Working
+     * @return
+     * @throws SQLException
+     */
+    static public Connection getConnectionToWorkingDB() throws SQLException {
+        if(pooledConToWorkingDB != null)
+            return pooledConToWorkingDB.getConnection();
+
+        return null;
     }
 
 
-
-
-
-
-
-
-//
-//    public int checkDostup( int ID, String passwd) throws SQLException {
-//        int dostup=0;
-//        String TERMINAL_ID = GetTerminalString(ID);
-//        String SQLText = "SELECT ADDRES, ID, BANK_ID FROM TERMINAL WHERE\n" +
-//                "    TERMINAL_ID=\'"+TERMINAL_ID+"\' AND\n"+
-//                "    BANK_ID =(SELECT BANK FROM USERS WHERE LOGIN=\'"+passwd+"\') AND\n" +
-//                "    (SELECT count(*) FROM SMENA WHERE DATA_K is null and SMENA.id_terminal=TERMINAL.ID)>0";
-//        ResultSet rs = DB_TERMINAL.resultQuery(SQLText);
-//        while (rs.next()) {
-//            dostup = rs.getInt("ID");//Integer.getInteger(rs.getString("ID"));
-//        }
-//        return dostup;
-//    }
-//
-//    private String GetTerminalString(int ID) throws SQLException {
-//        String ID_TERMINAL="";
-//        //System.out.println("ID_TERMINAL="+ID_TERMINAL);
-//
-//        String SQLText = "SELECT TERMINAL_ID, ID FROM TERMINAL WHERE id=" + String.valueOf(ID);
-//        ResultSet rs = DB_TERMINAL.resultQuery(SQLText);
-//        while (rs.next()) {
-//            ID_TERMINAL = rs.getString("TERMINAL_ID");
-//        }
-//        return ID_TERMINAL;
-//    }
 
 }
 
