895c898e6e5a11be7e399606c3b6e32546179450
marmusevich <myrmik@gmail.com>
2017-01-02 16:45:29 +0200

    CommandCloseOplata

--- NWProtocol/src/main/java/protocol/commands/CommandCloseOplata.java
+++ NWProtocol/src/main/java/protocol/commands/CommandCloseOplata.java
@@ -4,25 +4,27 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 
-//Команда closeoplata
-//        Выполняет закрытие процедуры оплаты. Значение.
-//        4.	closeoplata при успешном выполнении возвращает CLOSEOPL
-//        5.	Возвращение результата выполнения команды
-//        В случае успешного выполнения команды возвращается 200 ОК, в случае возникновения какой либо ошибки выводится сообщение 500 ERROR. По завершению работы команды происходит отключение от сервера.
-//        Наименования параметров:
-//        ID_TERMINAL = Идентификатор терминала, обязательный параметр;
-//        LOGIN = Выданный логин, обязательный параметр;
-//
-//        Количество передаваемых параметров – два. В случае неправильного написания наименования параметров, параметр будет проигнорирован, и заполнен значением по умолчанию. В случае не заполнения одного из обязательных параметров сервер вернет ошибку выполнения команды.  Параметры могут быть перечислены в любой последовательности.
-//
-//        6.	Возвращение результата выполнения команды
-//        В случае успешного выполнения команды возвращается 200 ОК, в случае возникновения какой либо ошибки выводится сообщение 500 ERROR. По завершению работы команды происходит отключение от сервера.
-
-
 /**
- * Created by lexa on 08.12.2016.
+ * Команда closeoplata
+ * Выполняет закрытие процедуры оплаты. Значение.
+ * 4.	closeoplata при успешном выполнении возвращает CLOSEOPL
+ * 5.	Возвращение результата выполнения команды
+ * В случае успешного выполнения команды возвращается 200 ОК, в случае возникновения какой либо ошибки выводится сообщение 500 ERROR.
+ * По завершению работы команды происходит отключение от сервера.
+ * Наименования параметров:
+ * ID_TERMINAL = Идентификатор терминала, обязательный параметр;
+ * LOGIN = Выданный логин, обязательный параметр;
+ *
+ * Количество передаваемых параметров – два. В случае неправильного написания наименования параметров, параметр будет проигнорирован,
+ * и заполнен значением по умолчанию. В случае не заполнения одного из обязательных параметров сервер вернет ошибку выполнения команды.
+ * Параметры могут быть перечислены в любой последовательности.
+ * 6.	Возвращение результата выполнения команды
+ * В случае успешного выполнения команды возвращается 200 ОК, в случае возникновения какой либо ошибки выводится сообщение 500 ERROR. По завершению работы команды происходит отключение от сервера.
  */
 public class CommandCloseOplata extends AbstractCommand {
     private static final Logger logger = LoggerFactory.getLogger(CommandCloseOplata.class);
@@ -30,75 +32,44 @@ public class CommandCloseOplata extends AbstractCommand {
     /**
      * первый ответ
      */
-    public static final String firstResponse = "";
+    public static final String firstResponse = "CLOSEOPL";
 
     /**
      * попытатся распарсить данные команды
      * @param commandData
      */
     public static CommandCloseOplata tryParseCommand(String commandData) {
-        CommandData ret = null;
+        CommandCloseOplata ret = null;
         boolean flOK = false;
 
         UserAuthenticationData uad = new UserAuthenticationData();
         flOK = Parser.parseUserAndPassword(commandData, uad);
 
         if (flOK) {
-            ret = new CommandData();
+            ret = new CommandCloseOplata();
             ret.setUserNameAndPass(uad);
         }
-//////////////////////////////////////////////////////////////////////
-//        else if SameText(trim(LCmd), 'closeoplata') then
-//        begin
-//        AContext.Connection.Socket.WriteLn('CLOSEOPL',TEncoding.UTF8);
-//        AContext.Connection.Socket.ReadStrings(Str,2,TEncoding.UTF8);
-//        {Подключение функции проверки и занесения данных в базу}
-//        AContext.Connection.Socket.WriteLn(DM1.CloseOplata(Str,AContext.Connection.Socket.Binding.PeerIP,LOGIN,PASSWD,DB),TEncoding.UTF8);
-//        AContext.Connection.Socket.Close;
-//        end
-
-
-        return null;
+        return ret;
     }
 
 
-
     @Override
-    public void doWorck(ArrayList<String> result, Connection connectionToTerminalDB, Connection connectionToWorkingDB) {
+    public void doWorck(ArrayList<String> result, Connection connectionToTerminalDB, Connection connectionToWorkingDB) throws SQLException {
 
-////Закрытие процедуры оплаты
-////Процедуру будет отключена как устаревшая
-//        function TDM1.CloseOplata(DATA: TStringList;IPer:string;USER:string;PASSWD:string;DB:string):string;
-//        var
-//        Str:TStringList;
-//        begin
-//        Result:='500 ERROR';
-//        if ConnectFIB(USER,PASSWD,DB) then
-//        BEGIN
-//        Str:=TStringList.Create;
-//        //Проверка корректности подключения терминала подключение возможно только после открытия смены
-//        //Если сессия открыта для данного терминала
-//        if (TerminalID(DATA)>0) THEN
-//        BEGIN
-//        {//Определим значение генератора пачек денег кешкодера
-//        sqlNew.Close;
-//        sqlNew.SQL.Clear;
-//        sqlNew.SQL.Add('select gen_id(GEN_SETCURRENCE, 1) from rdb$database');
-//        try
-//        sqlNew.Transaction.StartTransaction;
-//        sqlNew.ExecQuery;
-//        sqlNew.Transaction.CommitRetaining; }
-//        Result:='200 OK';
-//        {except
-//        Str.Add('Error exec select gen_id(GEN_SETCURRENCE, 1) from rdb$database');
-//        Result:='500 Error SET_GEN';
-//        Exit;
-//        end; }
-//        END ELSE BEGIN Result:='500 Error open smen'; END;
-//        Str.Free;
-//        END ELSE Result:='500 Error connect FIB';
-//        end;
-//
+        String SQLText = "select gen_id(GEN_SETCURRENCE, 1) from rdb$database";
+        try {
+            connectionToTerminalDB.setAutoCommit(false);
+            PreparedStatement ps = connectionToTerminalDB.prepareStatement(SQLText);
+            ResultSet rs = ps.executeQuery();
+            connectionToTerminalDB.commit();
+
+        } catch (SQLException e) {
+            connectionToTerminalDB.rollback();
+            connectionToTerminalDB.setAutoCommit(true);
+            throw e;
+        } finally {
+            connectionToTerminalDB.setAutoCommit(true);
+        }
     }
 }
 
