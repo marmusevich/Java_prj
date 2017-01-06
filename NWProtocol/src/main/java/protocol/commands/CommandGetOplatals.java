94e841629a022c27847d92f88a492702fe5032cf
marmusevich <myrmik@gmail.com>
2017-01-06 15:15:09 +0200

    CommandGetOplatals

--- NWProtocol/src/main/java/protocol/commands/CommandGetOplatals.java
+++ NWProtocol/src/main/java/protocol/commands/CommandGetOplatals.java
@@ -3,90 +3,97 @@ package protocol.commands;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
+import java.text.ParsePosition;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
-//Команда getoplatals
-//        Выполняет процедуру получения данных оплат по лицевому счету получателя LS_POLUCH, в формате TString (массив строк)
-//        1.	getoplatals при успешном выполнении возвращает GOPLATALS и ожидает передачи данных в формате TString (массив строк)
-//        2.	Передача переменной количества параметров в списке TString (массив строк) передается числовым значением.
-//        3.	Передача параметров в формате TString (массив строк)
-//        Наименования параметров:
-//        ID_TERMINAL = Идентификатор терминала, обязательный параметр;
-//        LOGIN = Выданный логин, обязательный параметр;
-//        LS_POLUCH= лицевой счет получателя платежа;
-//        DATA = Дата от которой начала периода выбора платежей
-//        KOD_ORG = Код организации поставщика услуг
-//
-//        В случае неправильного написание наименования параметров, параметр будет проигнорирован, и заполнен значением по умолчанию. В случае не заполнения одного из обязательных параметров сервер вернет ошибку выполнения команды.  Параметры могут быть перечислены в любой последовательности.
-//
-//        4.	Далее сервер возвращает число количества строк в возвращаемом параметре TString (массив строк)
-//        5.	После возвращает значение TString (массив строк) с заполненными данными, которые представляются в виде значений разделенными вертикальной чертой “|”, (Значение|Значение1|Значение2 и т.д.) . Значение колонок по порядку:
-//        •	Идентификатор платежа
-//        •	ФИО
-//        •	Адрес,
-//        •	Сумма,
-//        •	Индивидуальный налоговый номер,
-//        •	Номер постановления,
-//        •	Дата постановления,
-//        •	Код организации,
-//        •	Код организации подотчетный,
-//        •	Код смены,
-//        •	Идентификатор терминала,
-//        •	Код услуги,
-//        •	IP,
-//        •	Идентификатор купюроприемника,
-//        •	Дата,
-//        •	Дата вноса
-//        •	Дата изменения
-//        •	Кто вносил
-//        •	Кто изменял,
-//        •	Начальный период,
-//        •	Конечный период,
-//        •	Лицевой счет единый,
-//        •	Лицевой счет получателя,
-//        •	Комиссия,
-//        •	Комиссия 2,
-//        •	Название организации получателя,
-//        •	МФО Получателя,
-//        •	ОКПО Получателя,
-//        •	Банк получателя,
-//        •	Расчетный счет получателя,
-//        •	Идентификатор сторно,
-//        •	Комиссия 3,
-//        •	Комиссия 4
-
-
 /**
- * Created by lexa on 08.12.2016.
+ * Команда getoplatals
+ *         Выполняет процедуру получения данных оплат по лицевому счету получателя LS_POLUCH, в формате TString (массив строк)
+ *         1.	getoplatals при успешном выполнении возвращает GOPLATALS и ожидает передачи данных в формате TString (массив строк)
+ *         2.	Передача переменной количества параметров в списке TString (массив строк) передается числовым значением.
+ *         3.	Передача параметров в формате TString (массив строк)
+ *         Наименования параметров:
+ *         ID_TERMINAL = Идентификатор терминала, обязательный параметр;
+ *         LOGIN = Выданный логин, обязательный параметр;
+ *         LS_POLUCH= лицевой счет получателя платежа;
+ *         DATA = Дата от которой начала периода выбора платежей
+ *         KOD_ORG = Код организации поставщика услуг
+ *         В случае неправильного написание наименования параметров, параметр будет проигнорирован, и заполнен значением по умолчанию.
+ *         В случае не заполнения одного из обязательных параметров сервер вернет ошибку выполнения команды.
+ *         Параметры могут быть перечислены в любой последовательности.
+ *         4.	Далее сервер возвращает число количества строк в возвращаемом параметре TString (массив строк)
+ *         5.	После возвращает значение TString (массив строк) с заполненными данными, которые представляются в виде
+ *         значений разделенными вертикальной чертой “|”, (Значение|Значение1|Значение2 и т.д.) . Значение колонок по порядку:
+ *         •	Идентификатор платежа
+ *         •	ФИО
+ *         •	Адрес,
+ *         •	Сумма,
+ *         •	Индивидуальный налоговый номер,
+ *         •	Номер постановления,
+ *         •	Дата постановления,
+ *         •	Код организации,
+ *         •	Код организации подотчетный,
+ *         •	Код смены,
+ *         •	Идентификатор терминала,
+ *         •	Код услуги,
+ *         •	IP,
+ *         •	Идентификатор купюроприемника,
+ *         •	Дата,
+ *         •	Дата вноса
+ *         •	Дата изменения
+ *         •	Кто вносил
+ *         •	Кто изменял,
+ *         •	Начальный период,
+ *         •	Конечный период,
+ *         •	Лицевой счет единый,
+ *         •	Лицевой счет получателя,
+ *         •	Комиссия,
+ *         •	Комиссия 2,
+ *         •	Название организации получателя,
+ *         •	МФО Получателя,
+ *         •	ОКПО Получателя,
+ *         •	Банк получателя,
+ *         •	Расчетный счет получателя,
+ *         •	Идентификатор сторно,
+ *         •	Комиссия 3,
+ *         •	Комиссия 4
  */
 public class CommandGetOplatals extends AbstractCommand {
     private static final Logger logger = LoggerFactory.getLogger(CommandGetOplatals.class);
 
-
     /**
      * первый ответ
      */
-    public static final String firstResponse = "";
+    public static final String firstResponse = "GOPLATALS";
 
     /**
      * попытатся распарсить данные команды
      * @param commandData
      */
     public static CommandGetOplatals tryParseCommand(String commandData) {
-        CommandData ret = null;
+        CommandGetOplatals ret = null;
         boolean flOK = false;
 
         UserAuthenticationData uad = new UserAuthenticationData();
         flOK = Parser.parseUserAndPassword(commandData, uad);
 
+        String _ls_poluch = Parser.getParametrData(commandData, "LS_POLUCH");
+        String _data = Parser.getParametrData(commandData, "DATA");
+        String _kod_org = Parser.getParametrData(commandData, "KOD_ORG");
+
+        flOK = flOK && (_ls_poluch != null) && (_data != null) && (_kod_org != null);
+
         if (flOK) {
-            ret = new CommandData();
+            ret = new CommandGetOplatals();
             ret.setUserNameAndPass(uad);
+            ret.ls_poluch = _ls_poluch;
+            SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
+            ret.data = dateFormat.parse(_data, new ParsePosition(0));
+            ret.kod_org = Long.parseLong(_kod_org);
         }
+        return ret;
 
 ///////////////////////////////////////////////////////////////////////////////
 //        else if SameText(trim(LCmd), 'getoplatals') then
@@ -124,22 +131,43 @@ public class CommandGetOplatals extends AbstractCommand {
 //        AContext.Connection.Disconnect;
 //        end
 
-        return null;
     }
 
+
+    String ls_poluch = "";
+    java.util.Date data = null;
+    long kod_org= -1;
 
     @Override
     public void doWorck(ArrayList<String> result, Connection connectionToTerminalDB, Connection connectionToWorkingDB) throws SQLException {
         String SQLText =
-                "  " +
-                        "  ";
+                " SELECT * FROM oplata WHERE " +
+                        " OPLATA.LS_POLUCH LIKE ? and DATA_V>=? and KOD_ORG=? ";
 
         PreparedStatement ps = connectionToTerminalDB.prepareStatement(SQLText);
-        ps.setString(1, userAuthenticationData.name);
+        ps.setString(1, ls_poluch);
+        ps.setDate(1, (Date) data);
+        ps.setLong(1, kod_org);
         ResultSet rs = ps.executeQuery();
+        SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
+        ResultSetMetaData rsm = rs.getMetaData();
         while (rs.next()) {
-            //dostup = rs.getInt("ID");//Integer.getInteger(rs.getString("ID"));
-            //System.out.println("dostup=" + dostup +     " -> ADDRES = " + rs.getString("ADDRES") + ", ID = " + rs.getString("ID") + "BANK_ID = " + rs.getString("BANK_ID"));
+            String tmp = "";
+            for(int i = 0; i <= rsm.getColumnCount(); i++) {
+                if(tmp != ""){
+                    // todo DATE - это тип данных фаерберд
+                    if(rsm.getColumnTypeName(i) != "DATE")
+                        tmp += "|" + rs.getString(i).trim();
+                    else // DATE
+                        tmp += "|" + dateFormat.format(rs.getDate(i));
+                }
+                else { // first row
+                    if(rsm.getColumnTypeName(i) != "DATE")
+                        tmp += rs.getString(i).trim();
+                    else // DATE
+                        tmp += dateFormat.format(rs.getDate(i));
+                }
+            }
         }
 
 
