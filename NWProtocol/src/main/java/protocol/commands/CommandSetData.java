60a3b0d23af523497244d03444f08dac70329f3a
marmusevich <myrmik@gmail.com>
2017-01-06 22:52:45 +0200

    CommandSetData

--- NWProtocol/src/main/java/protocol/commands/CommandSetData.java
+++ NWProtocol/src/main/java/protocol/commands/CommandSetData.java
@@ -3,9 +3,11 @@ package protocol.commands;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.SQLException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.sql.*;
+import java.text.ParsePosition;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 
 
@@ -76,9 +78,70 @@ public class CommandSetData extends AbstractCommand {
         UserAuthenticationData uad = new UserAuthenticationData();
         flOK = Parser.parseUserAndPassword(commandData, uad);
 
+        String _id_currence = Parser.getParametrData(commandData, "ID_CURRENCE");
+        String _addres = Parser.getParametrData(commandData, "ADDRES");
+        String _fio = Parser.getParametrData(commandData, "FIO");
+        String _summa = Parser.getParametrData(commandData, "SUMMA");
+        String _inn = Parser.getParametrData(commandData, "INN");
+        String _postanova = Parser.getParametrData(commandData, "POSTANOVA");
+        String _data_postanov = Parser.getParametrData(commandData, "DATA_POSTANOV");
+        String _kod_org = Parser.getParametrData(commandData, "KOD_ORG");
+        String _id_terminal = Parser.getParametrData(commandData, "ID");
+        String _usluga = Parser.getParametrData(commandData, "USLUGA");
+
+        String _data = Parser.getParametrData(commandData, "DATA");
+        String _kto_v = Parser.getParametrData(commandData, "LOGIN");
+        String _per_start = Parser.getParametrData(commandData, "PER_START");
+        String _per_end = Parser.getParametrData(commandData, "PER_END");
+        String _ls = Parser.getParametrData(commandData, "LS");
+        String _ls_poluch = Parser.getParametrData(commandData, "LS_POLUCH");
+        String _kod_org_dop = Parser.getParametrData(commandData, "KOD_ORG_DOP");
+        String _komisiya = Parser.getParametrData(commandData, "KOMISIYA");
+        String _komisiya_bank = Parser.getParametrData(commandData, "KOMISIYA_BANK");
+        String _organization = Parser.getParametrData(commandData, "ORGANIZATION");
+        String _mfo = Parser.getParametrData(commandData, "MFO");
+        String _okpo = Parser.getParametrData(commandData, "OKPO");
+        String _bank = Parser.getParametrData(commandData, "BANK");
+        String _r_shet = Parser.getParametrData(commandData, "R_SHET");
+
+        flOK = flOK && (_id_currence != null) && (_addres != null) && (_fio != null) &&
+            (_summa != null) && (_inn != null) && (_postanova != null) && (_data_postanov != null) &&
+            (_kod_org != null) && (_id_terminal != null) && (_usluga != null) && (_data  != null) &&
+            (_kto_v != null) && (_per_start != null) && (_per_end != null) && (_ls != null) &&
+            (_ls_poluch != null) && (_kod_org_dop != null) && (_komisiya != null) && (_komisiya_bank != null) &&
+            (_organization != null) && (_mfo != null) && (_okpo != null) && (_bank != null) && (_r_shet != null);
+
         if (flOK) {
             ret = new CommandSetData();
             ret.setUserNameAndPass(uad);
+            ret.id_currence = Long.parseLong(_id_currence);
+            ret.addres = _addres;
+            ret.fio = _fio;
+            ret.summa = Float.parseFloat(_summa);
+            ret.inn = _inn;
+            ret.postanova = _postanova;
+            SimpleDateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy HH:mm:ss");
+            ret.data_postanov = dateFormat.parse(_data_postanov, new ParsePosition(0)); //todo поотенциально проблема распарсивания
+            ret.kod_org = Integer.parseInt(_kod_org);
+            ret.id_terminal = Integer.parseInt(_id_terminal);
+            ret.usluga = Integer.parseInt(_usluga);
+
+            //String ip = _ip;
+
+            ret.data = dateFormat.parse(_data, new ParsePosition(0)); //todo поотенциально проблема распарсивания
+            ret.kto_v = _kto_v;
+            ret.per_start = _per_start;
+            ret.per_end = _per_end;
+            ret.ls = Integer.parseInt(_ls);
+            String ls_poluch = _ls_poluch;
+            ret.kod_org_dop = Integer.parseInt(_kod_org_dop);
+            ret.komisiya = Float.parseFloat(_komisiya);
+            ret.komisiya_bank = Float.parseFloat(_komisiya_bank);
+            ret.organization = _organization;
+            ret.mfo = _mfo;
+            ret.okpo = _okpo;
+            ret.bank = _bank;
+            ret.r_shet = _r_shet;
         }
 
         return ret;
@@ -105,17 +168,81 @@ public class CommandSetData extends AbstractCommand {
 //        end
     }
 
+    long id_currence =0;
+    String addres = "";
+    String fio = "";
+    float summa = 0;
+    String inn = "";
+    String postanova = "";
+    java.util.Date data_postanov = null;
+    int kod_org = 0;
+    int id_terminal = 0;
+    int usluga = 0;
+    String ip = "";
+    java.util.Date data = null;
+    String kto_v = "";
+    String per_start = "";
+    String per_end = "";
+    int ls = 0;
+    String ls_poluch = "";
+    int kod_org_dop = 0;
+    float komisiya = 0;
+    float komisiya_bank = 0;
+    String organization = "";
+    String mfo = "";
+    String okpo = "";
+    String bank = "";
+    String r_shet = "";
 
     @Override
     public void doWorck(ArrayList<String> result, Connection connectionToTerminalDB, Connection connectionToWorkingDB) throws SQLException {
-        String SQLText =
-                "  ";
+        String SQLText = " INSERT INTO OPLATA (ID_CURRENCE ,ADDRES, FIO, SUMMA, INN, POSTANOVA, DATA_POSTANOV, " +
+                " KOD_ORG, ID_TERMINAL, USLUGA, DATA, IP, KTO_V, PER_START, PER_END, LS, LS_POLUCH, KOD_ORG_DOP, " +
+                "KOMISIYA, KOMISIYA_BANK, ORGANIZATION, MFO, OKPO, BANK, R_SHET) " +
+                " VALUES ( ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ? ) ";
+//        sqlNew.SQL.Add('(:ID_CURRENCE, :ADDRES, :FIO, :SUMMA, :INN, :POSTANOVA, :DATA_POSTANOV, :KOD_ORG, :ID_TERMINAL, :USLUGA, :DATA, :IP, :KTO_V, :PER_START, :PER_END, :LS, :LS_POLUCH, :KOD_ORG_DOP, :KOMISIYA, :KOMISIYA_BANK, :ORGANIZATION, :MFO, :OKPO, :BANK, :R_SHET)');
+
+        InetAddress remoteAddress = ((InetSocketAddress)ctx.pipeline().channel().remoteAddress()).getAddress();
+        ip = remoteAddress.getHostAddress();
+
 
         PreparedStatement ps = connectionToTerminalDB.prepareStatement(SQLText);
-        ps.setString(1, userAuthenticationData.name);
-        int countChangeString = ps.executeUpdate();
-        if(countChangeString != -1) { // ok
 
+        ps.setLong(1, id_currence );
+        ps.setString(2, addres );
+        ps.setString(3, fio );
+        ps.setFloat(4, summa );
+        ps.setString(5, inn );
+        ps.setString(6, postanova );
+        ps.setDate(7, (Date) data_postanov );
+        ps.setInt(8, kod_org );
+        ps.setInt(9, id_terminal );
+        ps.setInt(10, usluga );
+        ps.setString(11, ip );
+        ps.setDate(12, (Date) data );
+        ps.setString(13, kto_v );
+        ps.setString(14, per_start );
+        ps.setString(15, per_end );
+        ps.setInt(16, ls );
+        ps.setString(17, ls_poluch );
+        ps.setInt(18, kod_org_dop );
+        ps.setFloat(19, komisiya );
+        ps.setFloat(20, komisiya_bank );
+        ps.setString(21, organization );
+        ps.setString(22, mfo );
+        ps.setString(23, okpo );
+        ps.setString(24, bank );
+        ps.setString(25, r_shet );
+
+        int countChangeString = ps.executeUpdate();
+        ps.close();
+
+        if(countChangeString != -1) { // ok
+            SQLText = "select gen_id(GEN_OPLATA_ID, 0) from rdb$database ";
+            ps = connectionToTerminalDB.prepareStatement(SQLText);
+            ResultSet rs = ps.executeQuery();
+            rs.next();
+            result.add("PAY_ID=" + rs.getString("GEN_ID"));
         }
         else { //error
             result.add("500 Error insert record");
