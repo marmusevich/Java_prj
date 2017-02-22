1d1022c812817a2447dd7ac00b357022e7516a01
marmusevich <myrmik@gmail.com>
2017-02-22 16:15:07 +0200

    *

---
+++ ImportDKtoIT/src/main/java/myrmik/StartPoint.java
@@ -0,0 +1,18 @@
+package myrmik;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ * Created by asus on 22.02.2017.
+ */
+public class StartPoint {
+    private static final Logger logger = LoggerFactory.getLogger(StartPoint.class);
+
+
+    public static void main(String[] args) {
+        logger.info(" Before start server...");
+
+    }
+}
